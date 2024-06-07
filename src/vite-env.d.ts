7294ec081d3c999bbdb969d6813f2291ca6dbd2b
Huy Bui <huy03081999@gmail.com>
2024-06-07 12:48:39 +0700

    add: virtual pwa register types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,12 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pwa/react" />
+
+declare module 'virtual:pwa-register' {
+  import type { RegisterSWOptions } from 'vite-plugin-pwa/types';
+
+  export type { RegisterSWOptions };
+
+  export function registerSW(
+    options?: RegisterSWOptions
+  ): (reloadPage?: boolean) => Promise<void>;
+}
